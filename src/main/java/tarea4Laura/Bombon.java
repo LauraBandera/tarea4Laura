ba8748bc4e0b9aff5dad157047ce6e57fe0f1e67
Laura <laurabande10@gmail.com>
2020-12-08 16:28:53 +0100

    Añade constructores en clase inventada

--- src/main/java/tarea4Laura/Bombon.java
+++ src/main/java/tarea4Laura/Bombon.java
@@ -5,4 +5,17 @@ public class Bombon {
 	private String sabor;
 	private int cantidad;
 	private String forma;
+	
+	public Bombon(String sabor, int cantidad, String forma) {
+		super();
+		this.sabor = sabor;
+		this.cantidad = cantidad;
+		this.forma = forma;
+	}
+
+	public Bombon() {
+		super();
+	}
+	
+	
 }
